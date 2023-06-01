79488a62f35b80d983296e95deb17818ed09c15d
smartXmaster <hiroshiono0214@gmail.com>
2023-06-01 09:23:07 -0700

    udpated: projectData

--- src/projectData.js
+++ src/projectData.js
@@ -4,7 +4,7 @@ const projects = [
     image: "./images/getflair.png",
     description:
       "Flair eliminates manual and repetitive work for multifamily marketers and managers to increase occupancy, headache-free",
-    tools: ["ReactJs", "Tailwind css"],
+    tools: ["TypeScript", "Next", "Tailwind css", "Node"],
     // github: "https://github.com/codewithvk/React-simple-portfolio",
     link: "https://www.getflair.io",
   },
@@ -13,7 +13,14 @@ const projects = [
     image: "./images/cropperfinance.png",
     description:
       "Decentralized Ecosystem that Empowers Projects & Maximizes Yield to move Defi forward on Solana. Swap, Yield Farming, Staking, IDO Launchpad & more.",
-    tools: ["ReactJs", "Tailwind css"],
+    tools: [
+      "TypeScript",
+      "Rust",
+      "ReactJs",
+      "Tailwind css",
+      "Solana",
+      "Blockchain",
+    ],
     // github: "https://github.com/codewithvk/React-simple-portfolio",
     link: "https://cropper.finance",
   },
@@ -22,7 +29,7 @@ const projects = [
     image: "./images/perfectyourtrading.png",
     description:
       "Perfect Your Trading is an educational platform built to make you an excellent trader in the shortest possible time.",
-    tools: ["ReactJs", "Tailwind css"],
+    tools: ["TypeScript", "Next", "Chakra-UI", "Django"],
     // github: "https://github.com/codewithvk/React-simple-portfolio",
     link: "https://perfectyourtrading.com",
   },
@@ -31,7 +38,7 @@ const projects = [
     image: "./images/shmacked.png",
     description:
       "Fast Delivery. Shmacked offers unmatched convenience by delivering everything from snacks, drinks, nicotine, and alcohol in less than 15 minutes!",
-    tools: ["ReactJs", "Tailwind css"],
+    tools: ["JavaScript", "React", "Tailwind css", "Django"],
     // github: "https://github.com/codewithvk/React-simple-portfolio",
     link: "https://shmacked.com",
   },
